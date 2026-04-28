dc18e5ee096b0ca2351ac1d4b2a7a215fb72b205
Agustin Carrasco <asermax@gmail.com>
2026-04-28 17:34:05 -0300

    Remove audio article: Cognitive Security

--- listen/articles.js
+++ listen/articles.js
@@ -1,6 +1,5 @@
 const ARTICLES = [
     { slug: 'bet-on-juniors-ai', title: 'The Bet On Juniors Just Got Better', meta: 'Kent Beck · December 2025' },
     { slug: 'gas-city', title: 'Welcome to Gas City', meta: 'Steve Yegge · April 2026' },
-    { slug: 'cognitive-security', title: 'Cognitive Security', meta: 'Geoffrey Huntley · March 2026' },
     { slug: 'gas-town', title: 'Gas Town: from Clown Show to v1.0', meta: 'Steve Yegge · April 2026' }
 ];
