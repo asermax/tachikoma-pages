407bc29ec922221a26a1754ad7e29748c3aaa253
Agustin Carrasco <asermax@gmail.com>
2026-07-27 21:04:18 +0000

    Add audio article: Adding Herdr support to a Pi extension

--- listen/articles.js
+++ listen/articles.js
@@ -1,4 +1,5 @@
 const ARTICLES = [
+    { slug: 'adding-herdr-support-to-a-pi-extension', title: 'Adding Herdr support to a Pi extension', meta: 'Aliou Diouf · Jul 2026', time: '6:23' },
     { slug: 'should-still-design-code-humans', title: 'Should we still design code for humans?', meta: 'Valentina Servile · Jul 2026', time: '20:40' },
     { slug: 'how-coding-agents-read-your-code', title: 'How coding agents read your code (and how to write for them)', meta: 'Ben Vinegar · Jul 2026', time: '37:04' },
     { slug: 'protecting-floss-commons-from-llms', title: 'Protecting our FLOSS commons from LLMs', meta: 'Codeberg · Jul 2026', time: '13:28' },
