07119b099b9b9b0a3b4801186482223f757b087e
Agustin Carrasco <asermax@gmail.com>
2026-08-13 13:53:15 +0000

    Remove orphaned listen article remnants: how-to-keep-thinking

--- listen/articles.js
+++ listen/articles.js
@@ -12,7 +12,6 @@ const ARTICLES = [
     { slug: 'how-to-influence-politics', title: 'How I influence tech company politics', meta: 'Sean Goedecke · Aug 2026', time: '10:18' },
     { slug: 'seeing-like-a-software-company', title: 'Seeing like a software company', meta: 'Sean Goedecke · Aug 2026', time: '24:46' },
     { slug: 'code-was-never-the-hard-part-is-an-insult-to-all-programmers', title: '"Code was never the hard part" is an insult to all programmers', meta: 'Senko Rašić · Aug 2026', time: '8:31' },
-    { slug: 'how-to-keep-thinking', title: 'How to keep thinking', meta: 'Sean Goedecke · 2026', time: '5:21' },
     { slug: 'fragments-august-4-martin-fowler', title: 'Fragments: August 4', meta: 'Martin Fowler · Aug 2026', time: '9:03' },
     { slug: 'pi-minimal-and-performant', title: 'Pi, Minimal and Performant', meta: 'Earendil · Aug 2026', time: '5:54' },
     { slug: 'shape-of-things-to-come-part-1', title: 'The Shape of Things to Come, Part 1', meta: 'Steve Yegge · Aug 2026', time: '39:20' },
