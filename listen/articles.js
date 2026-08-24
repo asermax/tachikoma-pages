0f2fe5c7338081fc549f6e75a04040cdebc652bd
Agustin Carrasco <asermax@gmail.com>
2026-08-24 14:59:30 +0000

    Add audio article: How I Find Problems to Solve as a Staff Engineer

--- listen/articles.js
+++ listen/articles.js
@@ -1,4 +1,5 @@
 const ARTICLES = [
+    { slug: 'how-i-find-problems-staff-engineer', title: 'How I Find Problems to Solve as a Staff Engineer', meta: 'Lalit Maganti · Aug 2026', time: '10:04' },
     { slug: 'anger-anxiety-and-agency', title: 'Anger, Anxiety and Agency', meta: 'Armin Ronacher · Aug 2026', time: '5:12' },
     { slug: 'i-will-fucking-piledrive-you-if-you-mention-ai-again', title: 'I Will Fucking Piledrive You If You Mention AI Again', meta: 'Ludicity · Aug 2026', time: '23:41' },
     { slug: 'the-worlds-left-to-conquer', title: 'The Worlds Left To Conquer', meta: 'Ludicity · Aug 2026', time: '12:31' },
