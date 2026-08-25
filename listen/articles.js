1541ec0ea56752d09bd613968ddfaef7fd4db2a8
Agustin Carrasco <asermax@gmail.com>
2026-08-25 02:14:15 +0000

    Add audio article: SaaS Isn't Dead. Sameness Is.

--- listen/articles.js
+++ listen/articles.js
@@ -1,4 +1,5 @@
 const ARTICLES = [
+    { slug: 'saas-isnt-dead-sameness-is', title: 'SaaS Isn\'t Dead. Sameness Is.', meta: 'Chad Fowler · Aug 2026', time: '26:29' },
     { slug: 'how-i-find-problems-staff-engineer', title: 'How I Find Problems to Solve as a Staff Engineer', meta: 'Lalit Maganti · Aug 2026', time: '10:04' },
     { slug: 'anger-anxiety-and-agency', title: 'Anger, Anxiety and Agency', meta: 'Armin Ronacher · Aug 2026', time: '5:12' },
     { slug: 'i-will-fucking-piledrive-you-if-you-mention-ai-again', title: 'I Will Fucking Piledrive You If You Mention AI Again', meta: 'Ludicity · Aug 2026', time: '23:41' },
