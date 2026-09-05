9d837a19b746aa177f6c27d48f1ceb6033b39e12
Agustin Carrasco <asermax@gmail.com>
2026-09-05 02:59:47 +0000

    Remove audio article: Radical responsibility means treating people like tools

--- listen/articles.js
+++ listen/articles.js
@@ -1,6 +1,5 @@
 const ARTICLES = [
     { slug: 'freedom-and-resentment-peter-strawson', title: 'Freedom and Resentment', meta: 'Peter F. Strawson · 1962', time: '64:58' },
-    { slug: 'radical-responsibility-means-treating-people-like-tools', title: 'Radical responsibility means treating people like tools', meta: 'Sean Goedecke · Sep 2026', time: '3:32' },
     { slug: 'there-are-many-agent-harnesses-but-this-one-is-mine', title: 'There are many agent harnesses, but this one is mine.', meta: 'Jakob · Earendil', time: '4:10' },
     { slug: 'maybe-we-shouldnt-be-reviewing-all-this-code', title: 'Maybe We Shouldn\'t Be Reviewing All This Code', meta: 'Rachel Laycock · Aug 2026', time: '7:14' },
     { slug: 'how-to-protect-yourself-from-workslop', title: 'How to protect yourself from workslop', meta: 'Sean Goedecke · Sep 2026', time: '3:51' },
