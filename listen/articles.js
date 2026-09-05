06cd2054e826b9e7e8f1e19a58c63bcbedc4febe
Agustin Carrasco <asermax@gmail.com>
2026-09-05 04:54:09 +0000

    Remove audio article: Maybe We Shouldn't Be Reviewing All This Code

--- listen/articles.js
+++ listen/articles.js
@@ -1,7 +1,6 @@
 const ARTICLES = [
     { slug: 'freedom-and-resentment-peter-strawson', title: 'Freedom and Resentment', meta: 'Peter F. Strawson · 1962', time: '64:58' },
     { slug: 'there-are-many-agent-harnesses-but-this-one-is-mine', title: 'There are many agent harnesses, but this one is mine.', meta: 'Jakob · Earendil', time: '4:10' },
-    { slug: 'maybe-we-shouldnt-be-reviewing-all-this-code', title: 'Maybe We Shouldn\'t Be Reviewing All This Code', meta: 'Rachel Laycock · Aug 2026', time: '7:14' },
     { slug: 'how-to-protect-yourself-from-workslop', title: 'How to protect yourself from workslop', meta: 'Sean Goedecke · Sep 2026', time: '3:51' },
     { slug: 'how-software-factories-break-down-work', title: 'How Software Factories Break Down Work', meta: 'Josh Rosen · Aug 2026', time: '12:10' },
     { slug: 'agents-should-leave-receipts', title: 'Agents Should Leave Receipts', meta: 'Josh Rosen · Aug 2026', time: '11:31' },
