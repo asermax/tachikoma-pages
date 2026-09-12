7d4002521ff8c31f1fe8eca5e06dd4717c6a246f
Agustin Carrasco <asermax@gmail.com>
2026-09-12 20:40:02 +0000

    Add audio article: P(doom)

--- listen/articles.js
+++ listen/articles.js
@@ -1,4 +1,5 @@
 const ARTICLES = [
+    { slug: 'pdoom', title: 'P(doom)', meta: 'Armin Ronacher · Sep 2026', time: '9:16' },
     { slug: 'we-must-pace-the-frontier', title: 'We Must Pace the Frontier', meta: 'Dario Amodei · Sep 2026', time: '21:57' },
     { slug: 'i-made-a-build-profiler-to-understand-buns-compile-times', title: 'I made a build profiler to understand Bun\'s compile times', meta: 'Lalit Maganti · Sep 2026', time: '23:17' },
     { slug: 'dont-build-tools-for-ai-agents', title: 'Don\'t build tools for AI agents', meta: 'Sean Goedecke · Sep 2026', time: '4:30' },
