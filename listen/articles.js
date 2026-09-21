68358c0847619246770f82c596634f836d2227b9
Agustin Carrasco <asermax@gmail.com>
2026-09-21 18:59:49 +0000

    Add audio article: Caring vs. Knowing

--- listen/articles.js
+++ listen/articles.js
@@ -1,4 +1,5 @@
 const ARTICLES = [
+    { slug: 'caring-vs-knowing', title: 'Caring vs. Knowing', meta: 'Adam Zimman · Sep 2026', time: '6:44' },
     { slug: 'making-decisions-with-jev', title: 'Making decisions with Jev', meta: 'Nicolas Chagnet · Sep 20, 2026', time: '5:27' },
     { slug: 'grit-your-teeth-and-ship-it', title: 'Grit your teeth and ship it', meta: 'Sean Goedecke · Sep 2026', time: '5:57' },
     { slug: 'strategic-choices-when-both-options-are-good', title: 'Strategic choices: When both options are good', meta: 'Jason Cohen · Sep 2026', time: '18:44' },
